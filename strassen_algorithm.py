473c805a8d617fbd56970c158e2bcf0804541208
gabriel-fc <gfc@ic.ufal.br>
2019-06-14 21:02:56 -0300

    updating code

--- strassen_algorithm.py
+++ strassen_algorithm.py
@@ -62,24 +62,3 @@ def strassen(matrix_a, matrix_b, length):
     c22 = np.add(p5, np.subtract(np.subtract(p1, p3), p7))
 
     return set_c(c11, c12, c21, c22, int(length/2))  
-
-
-
-
-
-
-
-
-
-m1 = []
-m1.append([2, 0, 1, 0])
-m1.append([2, 1, 3, 0])
-m1.append([0, 0, 0, 0])
-m1.append([0, 0, 0, 0])
-m2 = []
-m2.append([1, 0, 0, 0])
-m2.append([3, -2, 0, 0])
-m2.append([2, 4, 0, 0])
-m2.append([0, 0, 0, 0])
-
-print(strassen(m1, m2, 4))
